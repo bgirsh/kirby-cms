69c5951b8b3a1b68764fe8ca23d8f221a81dd444
Barry Girsh <barrygirsh@gmail.com>
2026-06-19 11:52:05 -0400

    update config file

--- site/config/config.php
+++ site/config/config.php
@@ -1,10 +1,13 @@
 <?php
 
 return [
+    'url' => 'https://phpstack-941598-6489697.cloudwaysapps.com',
+
     'api' => [
         'basicAuth' => true,
         'allowInsecure' => true
     ],
+
     'kql' => [
         'auth' => false
     ]
